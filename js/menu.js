fcc63845f9cbec25d4e6fd1fb4519b62f623bb21
Disturbed2019 <disturbe@bk.ru>
2021-05-20 10:08:25 +0300

    day three: menu & modal window

--- js/menu.js
+++ js/menu.js
@@ -5,16 +5,10 @@ const toggleMenu = () =>{
     menuElem.classList.toggle('menu-active');
     burgerElem.classList.toggle('humburger-menu-active');
 };
-
-const closeMenu = () =>{
-    menuElem.classList.remove('menu-active');
-    burgerElem.classList.remove('humburger-menu-active');
-};
-
 burgerElem.addEventListener('click', toggleMenu);
 menuElem.addEventListener('click', event => {
     const target = event.target;
     if (target.classList.contains('menu-list__link') ){
-        closeMenu();
+        toggleMenu();
     }
 });
