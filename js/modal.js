70402ede59be39f89ee34db08108e85626f3814d
Disturbed2019 <disturbe@bk.ru>
2021-05-20 10:09:20 +0300

    day three: menu & modal window

--- js/modal.js
+++ js/modal.js
@@ -2,22 +2,17 @@ const  moreElem = document.querySelectorAll('.more');
 const modalElem = document.querySelector('.modal');
 
 const openModal = () => {
-modalElem.classList.remove('hidden');
+    modalElem.classList.remove('hidden');
 };
 const closeModal = () => {
     modalElem.classList.add('hidden');
-
 };
 moreElem.forEach(item => {
     item.addEventListener('click', openModal);
 });
-
 modalElem.addEventListener('click', event => {
     const target = event.target;
     if (target.classList.contains('overlay') || target.classList.contains('modal__close')){
         closeModal()
-    }
-    if (target.classList.contains('modal__title')){
-        target.style.color = 'red'
     }
 });
